7ea9ae853fd8c779564023a9f34ad831a2e1abd4
Michelle Martinez Bianchi <127590227+MichelleMyBad@users.noreply.github.com>
2023-11-20 23:52:28 +0100

    Add files via upload

---
+++ Rubrica/Contatto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubrica
+{
+    internal class Contatto
+    {
+        private int _numero;
+        private string _cognome;
+        private string _telefono;
+
+        public int Numero {
+            get {
+                return _numero;
+            }
+            set {
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException();
+
+                _numero = value; // value è il valore che gli passiamo, parola di c# che funziona in automatico  
+               
+            }
+        }
+
+        public string Nome { get; set; } // crea l'attributo senza esplicitarla
+        public string Cognome { get => _cognome; set => _cognome = value; }
+
+        //public string Test { get; private set; } //property di sola lettura
+        public string Telefono { get => _telefono; set => _telefono = value; }
+
+        public Contatto(int numero, string nome, string cognome, string telefono)
+        {
+            Numero = numero;
+            Nome = nome;
+            Cognome = cognome;
+            Telefono = telefono;
+        }
+
+        public Contatto() { }
+
+        //Costruisce contatto partendo da una riga csv
+        public Contatto (string riga)
+        {
+            string[] campi = riga.Split(';');
+
+
+            if (campi.Length >= 4)
+            {
+                int pk=0; //chiave identificativa
+                int.TryParse(campi[0], out pk);// prova a convertire la variabile e metterla in pk, se non è un intero mette 0  
+                this.Numero = pk;
+                this.Nome = campi[1];
+                this.Cognome = campi[2];
+                this.Telefono = campi[3];
+            }
+
+
+        }
+
+    }
+}
---
+++ Rubrica/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rubrica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Brush background;
+        public MainWindow() //costruttore di MainWindow
+        {
+            InitializeComponent(); //alloca tutte le risorse necessarie, costruisce tutta la grafica
+            background = dgDati.Background;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) //fa tutto dopo che viene caricata la grafica
+        {
+
+            int idx = 0;
+
+            try
+            {
+                const int MAX = 100;
+
+                StreamReader fin = new StreamReader("Dati.csv");
+                fin.ReadLine();
+
+                Contatto[] contatti = new Contatto[MAX];
+
+                while (!fin.EndOfStream)
+                {
+                    if (idx < MAX) // metto le parti del csv finchè ci stanno, se sono più di 100 non ci stanno più quindi devo smettere di inserirle se no va in errore
+                    {
+                        string riga = fin.ReadLine();
+                        Contatto c = new Contatto(riga);
+                        contatti[idx++] = c;
+                    }
+                    else { break; }
+                }
+
+                for (; idx < MAX; idx++) // creo i contatti vuoti se c'è ancora spazio
+                {
+                    Contatto c = new Contatto();
+                    contatti[idx] = c;
+                }
+
+                dgDati.ItemsSource = contatti;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Nono\n {ex.Message} alla riga numero {idx}");
+            }
+        }
+
+        private void dgDati_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            Contatto c = e.Row.Item as Contatto;//as ti mette l'item in c solo se è un contatto
+            if (c != null) {
+
+                if (c.Numero==0) { 
+                
+                    e.Row.Background = Brushes.Red;
+
+                }
+                else
+                {
+                    e.Row.Background= background;
+                }
+            
+            }
+        }
+    }
+}
